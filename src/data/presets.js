911f23aa6d84b4f1aa1cae55f59d915a83a14c7e
Zhong-ba <zhongbaz9@gmail.com>
2026-09-08 19:50:32 -0700

    update presets: use space character instead of spacing param, correct font choices, add shuttle and rear sign presets

--- src/data/presets.js
+++ src/data/presets.js
@@ -2,55 +2,71 @@ export const PRESETS = [
     {
         id: "titan-stacked",
         name: "Titan Stacked",
-        color: "#FF9000", width: "200", height: "24", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
-        route: "R5", routeFont: "24q", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
-        frames: [{ id: Date.now(), line1: "HASTINGS  ST", line1Font: "12d", line1Spacing: "6", line2: "TO KOOTENAY LOOP", line2Font: "9", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        color: "#FF9000", width: "200", height: "24", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        route: "R5", routeFont: "24qa1", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        frames: [{ id: Date.now(), line1: "H A S T I N G S  S T", line1Font: "12d", line1Spacing: "0", line2: "TO KOOTENAY LOOP", line2Font: "9", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
         id: "titan-unstacked",
         name: "Titan Unstacked",
-        color: "#FF9000", width: "200", height: "24", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
-        route: "11", routeFont: "24q", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        color: "#FF9000", width: "200", height: "24", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        route: "11", routeFont: "24qa1", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
         frames: [{ id: Date.now(), line1: "FIFA FAN FESTIVAL", line1Font: "16d", line1Spacing: "0", line2: "", line2Font: "9", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
         id: "horizon-stacked",
         name: "Horizon Stacked",
-        color: "#FF9000", width: "160", height: "16", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        color: "#FF9000", width: "160", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
         route: "119", routeFont: "16d", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
-        frames: [{ id: Date.now() + 4, line1: "KINGSWAY", line1Font: "8d", line1Spacing: "7", line2: "TO EDMONDS STN", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        frames: [{ id: Date.now() + 4, line1: "K I N G S W A Y", line1Font: "8d", line1Spacing: "0", line2: "TO EDMONDS STN", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
         id: "horizon-unstacked",
         name: "Horizon Unstacked",
-        color: "#FF9000", width: "160", height: "16", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        color: "#FF9000", width: "160", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
         route: "130", routeFont: "16d", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
         frames: [{ id: Date.now() + 4, line1: "METROTOWN STN", line1Font: "12d", line1Spacing: "0", line2: "", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
+        id: "shuttle-stacked",
+        name: "Shuttle Stacked",
+        color: "#FF9000", width: "120", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        route: "148", routeFont: "16d", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        frames: [{ id: Date.now() + 4, line1: "ROYAL OAK", line1Font: "7d", line1Spacing: "0", line2: "STN", line2Font: "7d", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        prFrames: []
+    },
+    {
+        id: "shuttle-unstacked",
+        name: "Shuttle Unstacked",
+        color: "#FF9000", width: "120", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        route: "31", routeFont: "16d", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        frames: [{ id: Date.now() + 4, line1: "RIVER DISTRICT", line1Font: "10dc", line1Spacing: "0", line2: "", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        prFrames: []
+    },
+    {
         id: "axion-stacked",
         name: "Axion Stacked",
-        color: "#FF9000", width: "160", height: "16", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        color: "#FF9000", width: "160", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
         route: "130", routeFont: "balios16", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
-        frames: [{ id: Date.now() + 4, line1: "WILLINGDON", line1Font: "balios8d", line1Spacing: "5", line2: "TO METROTOWN STN", line2Font: "balios7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        frames: [{ id: Date.now() + 4, line1: "W I L L I N G D O N", line1Font: "balios8d", line1Spacing: "0", line2: "TO METROTOWN STN", line2Font: "balios7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
         id: "axion-unstacked",
         name: "Axion Unstacked",
-        color: "#FF9000", width: "160", height: "16", speed: "2500", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
+        color: "#FF9000", width: "160", height: "16", speed: "1800", offColor: "#404040", ledShape: "square", ledSize: "3", ledGap: "1",
         route: "130", routeFont: "balios16", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
-        frames: [{ id: Date.now() + 4, line1: "METROTOWN STN", line1Font: "balios12", line1Spacing: "0", line2: "", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        frames: [{ id: Date.now() + 4, line1: "METROTOWN STN", line1Font: "balios12", line1Spacing: "0", line2: "", line2Font: "balios7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     },
     {
         id: "side1",
         name: "Side Sign (96x8)",
-        color: "#FF9000", width: "96", height: "8", speed: "2500", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
+        color: "#FF9000", width: "96", height: "8", speed: "1800", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
         route: "503", routeFont: "8", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
         frames: [{ id: Date.now() + 4, line1: "LANGLEY CTR", line1Font: "7", line1Spacing: "0", line2: "", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
@@ -58,9 +74,25 @@ export const PRESETS = [
     {
         id: "side2",
         name: "Side Sign (14x112)",
-        color: "#FF9000", width: "112", height: "14", speed: "2500", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
-        route: "19", routeFont: "14d", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        color: "#FF9000", width: "112", height: "14", speed: "1800", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
+        route: "19", routeFont: "14da1", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
         frames: [{ id: Date.now() + 4, line1: "STANLEY PARK", line1Font: "8d", line1Spacing: "0", line2: "", line2Font: "7", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        prFrames: []
+    },
+    {
+        id: "rear-conventional",
+        name: "Rear Conventional",
+        color: "#FF9000", width: "48", height: "16", speed: "1800", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
+        route: "", routeFont: "16t", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        frames: [{ id: Date.now() + 4, line1: "480", line1Font: "16t", line1Spacing: "0", line2: "", line2Font: "16t", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
+        prFrames: []
+    },
+    {
+        id: "rear-shuttle",
+        name: "Rear Shuttle",
+        color: "#FF9000", width: "48", height: "16", speed: "1800", offColor: "#404040", ledShape: "round", ledSize: "6", ledGap: "1",
+        route: "", routeFont: "16t", routeAlign: "LEFT", routeSuffix: "", routeSuffixFont: "16d",
+        frames: [{ id: Date.now() + 4, line1: "68", line1Font: "16d", line1Spacing: "0", line2: "", line2Font: "16d", line2Spacing: "0", animation: "NONE", animSpeed: "0.25", line1Align: "CENTRE", line2Align: "CENTRE", verticalSpacing: "FLUSH" }],
         prFrames: []
     }
 ];
